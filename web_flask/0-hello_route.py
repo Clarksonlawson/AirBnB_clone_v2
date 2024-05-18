5fb760785473aec6a7f651db63987f3a08b653f2
Awomu Clarkson <clarksonlawson1@gmail.com>
2024-05-18 15:21:47 +0000

    hi

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -13,6 +13,9 @@ def hello_hbnb():
     """Route that displays 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
